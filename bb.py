f67c920dcda9bcd8e948301171d1559ac6634485
Fasasi Abdulrahman Temitope <106965053+Ramond-king05@users.noreply.github.com>
2022-12-09 10:53:30 +0100

    Update bb.py

--- bb.py
+++ bb.py
@@ -1,4 +1,4 @@
 import streamlit as  st
 
 image=("MY.png")
-st.image(image,caption=None, width=500, use_column_width=1, clamp=False, channels="RGB", output_format="auto")
+st.image(image,caption=None, width=50, use_column_width=1, clamp=False, channels="RGB", output_format="auto")
